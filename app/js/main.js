cf0585ad0a4cf282b1629900cefe75e05ed53a2b
OrestPolusyn <opect.pol@i.ua>
2021-01-22 16:57:55 +0200

    start

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".slider__list").slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    lazyLoad: "progressive",
+    infinite: true,
+    arrows: false,
+    dots: true,
+    cssEase: "cubic-bezier(0.87, 0.03, 0.41, 0.9)",
+  });
+});
