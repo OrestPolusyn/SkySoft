37a1a688cc2c5b2a10dc539ed2fecf908258aea7
Орест Полусин <kingorest@gmail.com>
2021-04-04 23:04:15 +0300

    need to add blog page

--- app/js/main.js
+++ app/js/main.js
@@ -15,67 +15,103 @@ $(function () {
     $(this).siblings(".article-box__content").addClass("active");
     $(this).addClass("active");
   });
+  $(".info__block .info__item").on("click", function () {
+    $(".info__block .info__item").removeClass("info__item--active").eq($(this).index()).addClass("info__item--active");
+    $(".info__content-item").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("info__item--active");
+
+  $('.faq__tab').on("click", function () {
+    $(this).siblings(".faq__content").slideToggle();
+    $(this).children(".faq__icon").toggleClass('open');
+  });
+
+
 });
 
 var map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 49.22296138724512, lng: 31.856264242563697 },
+    center: {
+      lat: 49.22296138724512,
+      lng: 31.856264242563697
+    },
 
     zoom: 13,
 
-    styles: [
-      {
+    styles: [{
         featureType: "administrative",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#444444" }],
+        stylers: [{
+          color: "#444444"
+        }],
       },
       {
         featureType: "landscape",
         elementType: "all",
-        stylers: [{ color: "#f2f2f2" }],
+        stylers: [{
+          color: "#f2f2f2"
+        }],
       },
       {
         featureType: "poi",
         elementType: "all",
-        stylers: [{ visibility: "off" }],
+        stylers: [{
+          visibility: "off"
+        }],
       },
       {
         featureType: "road",
         elementType: "all",
-        stylers: [{ saturation: -100 }, { lightness: 45 }],
+        stylers: [{
+          saturation: -100
+        }, {
+          lightness: 45
+        }],
       },
       {
         featureType: "road.highway",
         elementType: "all",
-        stylers: [{ visibility: "simplified" }],
+        stylers: [{
+          visibility: "simplified"
+        }],
       },
       {
         featureType: "road.arterial",
         elementType: "labels.icon",
-        stylers: [{ visibility: "off" }],
+        stylers: [{
+          visibility: "off"
+        }],
       },
       {
         featureType: "transit",
         elementType: "all",
-        stylers: [{ visibility: "off" }],
+        stylers: [{
+          visibility: "off"
+        }],
       },
       {
         featureType: "water",
         elementType: "all",
-        stylers: [{ color: "#46bcec" }, { visibility: "on" }],
+        stylers: [{
+          color: "#46bcec"
+        }, {
+          visibility: "on"
+        }],
       },
     ],
   });
 
   var icon = {
-    url: "images/icons/map-marker-contact.svg", 
-    scaledSize: new google.maps.Size(50, 50), 
+    url: "images/icons/map-marker-contact.svg",
+    scaledSize: new google.maps.Size(50, 50),
   };
 
   var marker = new google.maps.Marker({
-    position: { lat: 49.22296138724512, lng: 31.856264242563697 },
+    position: {
+      lat: 49.22296138724512,
+      lng: 31.856264242563697
+    },
 
     map: map,
 
